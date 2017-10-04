a3a3706d052a4b28692a861dc69c763b08572b20
Sebastián Montoya Jiménez <sebastian.montoyaj@udea.edu.co>
2017-10-03 22:03:58 -0500

    Primer intento por hacer las vistas

--- Laboratorio2/AppPasarelaPago/AppPasarelaPago-web/src/main/java/com/udea/controller/CarritoMBean.java
+++ Laboratorio2/AppPasarelaPago/AppPasarelaPago-web/src/main/java/com/udea/controller/CarritoMBean.java
@@ -1,11 +1,51 @@
 package com.udea.controller;
 
-public class CarritoMBean
+import com.udea.modelo.Articulo;
+import com.udea.modelo.Transaccion;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.EJB;
+
+public class CarritoMBean implements Serializable
 {
 
-    public CarritoMBean()
-    {
+    @EJB
+    private com.udea.dao.ArticuloFacadeLocal articuloFacade;
         
+    private Articulo producto;
+    private List<Articulo> productos;
+    
+    public CarritoMBean(){
+        getProductos();
     }
     
+    public void refrescar()
+    {
+        productos = new ArrayList(articuloFacade.findAll());
+    }
+    
+    public List<Articulo> getProductos()
+    {
+        if ((productos == null) || (productos.isEmpty()))
+        {
+            refrescar();
+        }
+        System.out.println("entro");
+        return productos;
+    }
+    
+    
+    
+    
+    
+//    private String codigo;
+//    private String nombre;
+//    private String descripcion;
+//    private int existencia;
+//    private BigDecimal precioProveedor;
+//    private BigDecimal precioCliente;
+//    private Collection<Transaccion> transaccionCollection;
 }
