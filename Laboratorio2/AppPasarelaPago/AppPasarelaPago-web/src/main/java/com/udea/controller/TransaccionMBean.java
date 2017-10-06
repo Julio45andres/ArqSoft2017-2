5039baf10dea9448173accbe5164d4dfd3a16692
César Muñoz Roldan <cesar.munozr@udea.edu.co>
2017-10-06 16:44:13 -0500

    Se realiza ajustes en las vistas

--- Laboratorio2/AppPasarelaPago/AppPasarelaPago-web/src/main/java/com/udea/controller/TransaccionMBean.java
+++ Laboratorio2/AppPasarelaPago/AppPasarelaPago-web/src/main/java/com/udea/controller/TransaccionMBean.java
@@ -7,7 +7,11 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.validation.ConstraintViolationException;
 
 public class TransaccionMBean implements Serializable
@@ -21,7 +25,7 @@ public class TransaccionMBean implements Serializable
     
     private List<String> canasta = new ArrayList<>();
     private int numItemsEnCanasta;
-    private Transaccion transaccion;
+    //private Transaccion transaccion;
     
     private String duiCliente;
     private String nombreCliente;
@@ -33,20 +37,25 @@ public class TransaccionMBean implements Serializable
     private BigDecimal valorTotal;
     
     
-    private boolean disabled;
+    //Variables auxiliares para controlar la vista
+    private UIComponent mybutton;
+    private Locale locale=FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    private boolean disabled=true;
+    private String sSubCadena;
+    private String mensajeCard;
+    private String m;
     
     public TransaccionMBean()
     {
         numItemsEnCanasta = 0;
-        duiCliente="";
-        nombreCliente="asd";
-        email="";
-        numTCredito="";
-        cvvTCredito="";
-        tipoTCredito="";
-        fVenceTCredito="";
-        valorTotal=BigDecimal.valueOf(0);
-        disabled=true;
+        duiCliente="1045021276";
+        nombreCliente="CESAR";
+        email="cesar@udea.com";
+        numTCredito="1111111111";
+        cvvTCredito="111111";
+        tipoTCredito="VISA";
+        fVenceTCredito="05/2020";
+        valorTotal=BigDecimal.valueOf(0);        
     }
     
     public List<String> getCanasta()
@@ -98,8 +107,8 @@ public class TransaccionMBean implements Serializable
     }
     
     public String pagar(){
-        try{
-            this.transaccion=new Transaccion();  
+        //try{
+            Transaccion transaccion=new Transaccion();  
             /*this.transaccion.setDuiCliente("1045021276");
             this.transaccion.setNombreCliente("cesar");
             this.transaccion.setEmail("cesar@udea.com");
@@ -109,26 +118,83 @@ public class TransaccionMBean implements Serializable
             this.transaccion.setFVenceTCredito("09/2018");
             this.transaccion.setValorTotal(BigDecimal.valueOf(2500));
             this.transaccionFacade.create(transaccion);*/
-            this.transaccion.setDuiCliente(duiCliente);
-            this.transaccion.setNombreCliente(nombreCliente);
-            this.transaccion.setEmail(email);
-            this.transaccion.setNumTCredito(numTCredito);
-            this.transaccion.setCvvTCredito(cvvTCredito);
-            this.transaccion.setTipoTCredito(tipoTCredito);
-            this.transaccion.setFVenceTCredito(fVenceTCredito);
-            this.transaccion.setValorTotal(valorTotal);
+            transaccion.setDuiCliente(duiCliente);
+            transaccion.setNombreCliente(nombreCliente);
+            transaccion.setEmail(email);
+            transaccion.setNumTCredito(numTCredito);
+            transaccion.setCvvTCredito(cvvTCredito);
+            transaccion.setTipoTCredito(tipoTCredito);
+            transaccion.setFVenceTCredito(fVenceTCredito);
+            transaccion.setValorTotal(valorTotal);
             this.transaccionFacade.create(transaccion);
-            System.out.println("Pago efectuado correctamente");
-        }
+            return "PAGAR";
+        /*}
         catch(ConstraintViolationException e){
-            System.out.println("Error"+e.getMessage());
+            return "Error"+e.getMessage();
         }
         catch(Exception e){
-            System.out.println("No fue posible generar la transacción");
-        }
-        return "PAGAR";
+            return "No fue posible generar la transacción"+e.getMessage();
+        }*/
     }
-
+    
+    public String validar(){
+        String sCadena;
+        sCadena=String.valueOf(numTCredito);
+        sSubCadena=sCadena.substring(0, 5);
+        int val=Integer.parseInt(sSubCadena);
+        if(val>=11111 && val<=22222){
+            FacesMessage message=new FacesMessage("Tarjeta American Express");
+            FacesContext context=FacesContext.getCurrentInstance();
+            context.addMessage(mybutton.getClientId(context), message);
+            mensajeCard="Es American Express";
+            disabled=false;
+            this.setMensajeCard(mensajeCard);
+            return this.getMensajeCard();
+        }
+        else if(val>=33334 && val<=44444){
+            FacesMessage message=new FacesMessage("Tarjeta Diners");
+            FacesContext context=FacesContext.getCurrentInstance();
+            context.addMessage(mybutton.getClientId(context), message);
+            mensajeCard="Es Diners";
+            disabled=false;
+            this.setMensajeCard(mensajeCard);
+            return this.getMensajeCard();
+        }
+        else if(val>=55555 && val<=66666){
+            FacesMessage message=new FacesMessage("Tarjeta Visa");
+            FacesContext context=FacesContext.getCurrentInstance();
+            context.addMessage(mybutton.getClientId(context), message);
+            mensajeCard="Es Visa";
+            disabled=false;
+            this.setMensajeCard(mensajeCard);
+            return this.getMensajeCard();
+        }
+        else if(val>=77777 && val<=88888){
+            FacesMessage message=new FacesMessage("Tarjeta Mastercard");
+            FacesContext context=FacesContext.getCurrentInstance();
+            context.addMessage(mybutton.getClientId(context), message);
+            mensajeCard="Es Mastercard";
+            disabled=false;
+            this.setMensajeCard(mensajeCard);
+            return this.getMensajeCard();
+        }
+        else{
+           FacesMessage message= new FacesMessage("Invalid card");
+           FacesContext context= FacesContext.getCurrentInstance();
+           context.addMessage(mybutton.getClientId(context), message);
+        }
+        return null;
+    }
+    
+    public String getLanguage(){
+        return locale.getLanguage();
+    }
+    public void changeLanguage(String language){
+        locale=new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+    
+    
     public String getDuiCliente() {
         return duiCliente;
     }
@@ -193,9 +259,7 @@ public class TransaccionMBean implements Serializable
         this.valorTotal = valorTotal;
     }
     
-    public void validar(){
-        this.disabled= !this.disabled;
-    }
+   
     /*
     public Transaccion getTransaccion() {
         return transaccion;
@@ -212,6 +276,64 @@ public class TransaccionMBean implements Serializable
 
     public void setDisabled(boolean disabled) {
         this.disabled = disabled;
+    }
+
+    public ComprobanteFacadeLocal getComprobanteFacade() {
+        return comprobanteFacade;
+    }
+
+    public void setComprobanteFacade(ComprobanteFacadeLocal comprobanteFacade) {
+        this.comprobanteFacade = comprobanteFacade;
+    }
+
+    public TransaccionFacadeLocal getTransaccionFacade() {
+        return transaccionFacade;
+    }
+
+    public void setTransaccionFacade(TransaccionFacadeLocal transaccionFacade) {
+        this.transaccionFacade = transaccionFacade;
+    }
+
+
+
+    public UIComponent getMybutton() {
+        return mybutton;
+    }
+
+    public void setMybutton(UIComponent mybutton) {
+        this.mybutton = mybutton;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public String getsSubCadena() {
+        return sSubCadena;
+    }
+
+    public void setsSubCadena(String sSubCadena) {
+        this.sSubCadena = sSubCadena;
+    }
+
+    public String getMensajeCard() {
+        return mensajeCard;
+    }
+
+    public void setMensajeCard(String mensajeCarf) {
+        this.mensajeCard = mensajeCarf;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public void setM(String m) {
+        this.m = m;
     }
     
    
