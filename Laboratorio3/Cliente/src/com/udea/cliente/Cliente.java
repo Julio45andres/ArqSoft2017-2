026d2e9440d9ce490c57962d588421dbdc5cb61f
Sebastián Montoya Jiménez <sebastian.montoyaj@udea.edu.co>
2017-10-12 16:37:09 -0500

    Se termino de implementar el servidor y se hizo una prueba de comunicacion con el cliente, la cual fue exitosa.

--- Laboratorio3/Cliente/src/com/udea/cliente/Cliente.java
+++ Laboratorio3/Cliente/src/com/udea/cliente/Cliente.java
@@ -1,10 +1,43 @@
 package com.udea.cliente;
 
+// Importes necesarios para la clase
+import com.edu.rmi.IFiguras;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Cliente
 {
-    public static void main(String[] args)
+    private static IFiguras clienteFiguras = null;
+    
+    private static boolean conectarAlServidor()
     {
+        try
+        {
+            Registry registro = LocateRegistry.getRegistry("127.0.0.1", 1099);
         
+            clienteFiguras = (IFiguras) registro.lookup("ServidorFiguras");
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+        
+        return true;
     }
     
+    public static void main(String[] args) throws Exception
+    {
+        if (conectarAlServidor())
+        {
+            System.out.println("Prueba 1 -> Area de un rectangulo de lados a=4 y b=3 :");
+            System.out.println(clienteFiguras.areaRectangulo(4D, 3D));
+            
+            System.out.println("Prueba 2 -> Perimetro de un rectangulo de lados a=4 y b=3 :");
+            System.out.println(clienteFiguras.permitroRectangulo(4D, 3D));
+        }
+        else
+        {
+            System.out.println("No se pudo establecer conexion con el servidor!");
+        }
+    }
 }
--- Laboratorio3/Servidor/src/com/udea/servidor/Servidor.java
+++ Laboratorio3/Servidor/src/com/udea/servidor/Servidor.java
@@ -1,47 +1,64 @@
 package com.udea.servidor;
 
 // Importes necesarios para la clase
-import com.edu.rmi.IFiguras;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-// Paginas que ayudaron con el desarrollo de este proyecto:
+// Paginas que nos ayudaron con el desarrollo de este proyecto:
 // 
 // Sencillo ejemplo de JAVA RMI
 // http://jcesarmoreno.blogspot.com.co/2016/04/sencillo-ejemplo-de-java-rmi.html
 // 
-// 
-// 
+// New Easy Tutorial for Java RMI using Eclipse
+// http://www.ejbtutorial.com/java-rmi/new-easy-tutorial-for-java-rmi-using-eclipse
+//
+// How to Create Java RMI using Netbeans IDE Part1 [El mejor - Sencillo y efectivo]
+// https://youtu.be/el2PHj-XD4o
+//
+// How to Create Java RMI using Netbeans Part2
+// https://youtu.be/9sUn6WVXgmM
 
-public class Servidor
+// Clase que registrara los procedimientos a ser invocados y atendera a los clientes
+// NOTA: Esta clase tambien hereda de UnicastRemoteObject con el fin de que se puedan
+//       generar de forma automatizada los stubs, ahorrandonos el esfuerzo de compilar
+//       las clases por consola y permitiendonos ejecutar el servidor desde el mismo
+//       NetBeans sin requerir el uso de rmiregistry para reservar los puertos.
+public class Servidor extends UnicastRemoteObject
 {
+    // Constructor de la clase. No tiene un proposito especial
+    public Servidor() throws RemoteException
+    {
+        super();
+    }
     
+    // Metodo principal del servidor
     public static void main(String[] args)
     {
         try
         {
+            // En primer lugar, se crea el objeto remoto con los procedimientos que invocaran los clientes
             CalculadoraFiguras ServidorFiguras = new CalculadoraFiguras();
             
-            IFiguras FigurasRemotas = (IFiguras) UnicastRemoteObject.exportObject(ServidorFiguras, 1099);
+            // Luego, se registra este objeto bajo el puerto 1099 y con el nombre: ServidorFiguras
+            Registry registro = LocateRegistry.createRegistry(1099);
+            registro.rebind("ServidorFiguras", ServidorFiguras);
             
-            LocateRegistry.createRegistry(0);
-            
-            Registry registro = LocateRegistry.getRegistry();
-            registro.rebind("ServidorFiguras", FigurasRemotas);
-            
+            // Tambien se emite un mensaje de que el servidor ya es operativo y cuando se desee parar el mismo, el administrador solo debe presionar ENTER
             System.out.println("Servidor listo...\nPara terminar presione enter:\n");
             System.in.read();
             
+            // Si se pasa aqui es porque se quiere terminar con la ejecucion del servidor por lo que se desvincula el objeto ServidorFiguras del servicio de nombres y se indica explicitamente que ya no es posible recibir llamadas
             registro.unbind("ServidorFiguras");
             UnicastRemoteObject.unexportObject(ServidorFiguras, true);
         }
         catch(Exception e) // En caso que se tenga problemas para iniciar el servidor ...
         {
             // Muestro un mensaje al respecto e imprimo el error
-            System.out.println("Se presento un problema la iniciar el servidor!");
+            System.out.println("Se presento un problema al iniciar el servidor!");
             System.out.println("---");
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
